54d06199da3e710d717a1454da1ae3d65f35db06
KlaiRayen <klairayen123@gmail.com>
2025-12-02 03:22:40 +0100

    Added request/response DTOs

---
+++ Models/DTOs/CalculCreditRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Softt365Test.Models.DTOs
+{
+    public class CalculCreditRequestDto
+    {
+        public decimal MontantAchat { get; set; }
+        public decimal FondsPropres { get; set; }
+        public int DureeMois { get; set; }
+        public decimal TauxAnnuel { get; set; }
+    }
+}
---
+++ Models/DTOs/CalculCreditResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Softt365Test.Models.DTOs
+{
+    public class CalculCreditResponseDto
+    {
+        public decimal MontantEmprunterBrut { get; set; }
+        public decimal FraisHypotheque { get; set; }
+        public decimal MontantEmprunterNet { get; set; }
+        public decimal TauxMensuel { get; set; }
+        public decimal Mensualite { get; set; }
+        public List<LigneAmortissementDto> TableauAmortissement { get; set; }
+    }
+}
